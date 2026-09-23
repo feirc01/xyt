1c1e4a49f7d2114b4b4814783c4ed48452a8c4dd
feirc01 <feirc@qq.com>
2026-09-23 15:21:30 +0800

    Update foodList.js

--- foodList.js
+++ foodList.js
@@ -3,7 +3,7 @@ export default [
     "id": "gf",
     "name": "盖饭",
     "children": [
-      { "id": 1, "name": "黑椒牛柳饭", "description": "嫩滑牛柳搭配黑椒汁，配时蔬" },
+      { "id": 1, "name": "黑椒牛柳饭", "description": "嫩滑牛柳搭配黑椒汁，配时蔬123" },
       { "id": 2, "name": "咖喱鸡肉饭", "description": "日式咖喱，鸡腿肉，土豆胡萝卜" }
     ]
   },
@@ -11,8 +11,9 @@ export default [
     "id": "xs",
     "name": "小食",
     "children": [
-      { "id": 3, "name": "炸鸡块", "description": "酥脆多汁，搭配番茄酱" },
-      { "id": 4, "name": "薯条", "description": "黄金脆薯，撒盐" }
+      { "id": 3, "name": "炸鸡块", "description": "酥脆多汁，搭配番茄酱123" },
+      { "id": 4, "name": "薯条", "description": "黄金脆薯，撒盐123" },
+      { "id": 5, "name": "薯条", "description": "黄金脆薯，撒盐123" },
     ]
   }
 ]
